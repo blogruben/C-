a98349ece768b60d0872119e3f9999072b2fa2b2
Ruben-R-C <Info@blogruben.com>
2022-12-24 11:43:21 +0100

    entrada de datos, tipos de datos, operaciones matematicas

---
+++ 02holaMundo.cpp
@@ -0,0 +1,14 @@
+//Programa HolaMundo salto de lineas en C++
+ 
+ #include<iostream>
+ 
+ using namespace std;//para no usar std::cout
+ 
+ int main(){
+    
+    cout<<"Hola Mundo !!!"<<endl;//salto de linea
+    cout<<"Hola Mundo !!\n";//salto de linea
+    cout<<"Hola Mundo !";
+
+    return 0;//obligatorio devolver un entero
+ }
---
+++ 03tiposDeDatos.cpp
@@ -0,0 +1,21 @@
+//Tipos de datos básicos de C++
+
+#include<iostream>
+
+ using namespace std;
+
+ int main (){
+    int entero = 12;//entero
+    float flotante = 18.92;//presicion 7 decimales
+    double masDecimales = 15.6432;//precion 15 decimales
+    char letra = 'a';//caracter
+    char nombre[] = "Ruben";
+
+    cout<<"entero   "<<entero<<endl;
+    cout<<"flotante "<<flotante<<endl;
+    cout<<"double   "<<masDecimales<<endl;
+    cout<<"letra    "<<letra<<endl;
+    cout<<"Me llamo "<<nombre<<endl;
+
+     return 0;
+ }
